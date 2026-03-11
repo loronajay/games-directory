b702edc4f93a6841eff09be01e8a9538920813fb
loronajay <leojaylorona@gmail.com>
2026-03-11 03:29:39 -0700

    create automated workflow scripts and added some template text files for solid continuation of workflow

---
+++ control_overrides.py
@@ -0,0 +1,20 @@
+GAME_OVERRIDES = {
+
+    "blade-and-block": {
+        "up": "j",
+        "b": "c",
+        "a": "w"
+    },
+
+    "apple-catcher": {
+        "b": "ArrowLeft",
+        "x": "ArrowRight"
+    },
+
+    "x-pong": {
+        "y": "k",
+        "a": ",",
+        "x": "ArrowDown"
+    }
+
+}
